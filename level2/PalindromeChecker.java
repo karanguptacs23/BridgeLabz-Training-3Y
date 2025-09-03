3f228dd9ec58d42467fdc667c6aee62083e2d60c
Karan Gupta <karan.gupta_cs23@gla.ac.in>
2025-09-03 16:50:40 +0530

    [Karan] add . Palindrome Checker

---
+++ level2/PalindromeChecker.java
@@ -0,0 +1,29 @@
+public class PalindromeChecker {
+    private String text;
+
+    public PalindromeChecker(String text) {
+        this.text = text;
+    }
+
+    public boolean isPalindrome() {
+        String cleaned = text.replaceAll("\\s+", "").toLowerCase();
+        int left = 0, right = cleaned.length() - 1;
+
+        while (left < right) {
+            if (cleaned.charAt(left) != cleaned.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public void display() {
+        if (isPalindrome()) {
+            System.out.println("\"" + text + "\" is a Palindrome.");
+        } else {
+            System.out.println("\"" + text + "\" is not a Palindrome.");
+        }
+    }
+}
