b8af6a7ed67f5f13c28bc4d0e236adf685eaab4b
Karan Gupta <karan.gupta_cs23@gla.ac.in>
2025-09-20 23:45:50 +0530

    added q9

---
+++ ExtrasOnString/MostFrequentChar.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class MostFrequentChar {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+        int[] freq = new int[256];
+        for (char c : str.toCharArray()) {
+            freq[c]++;
+        }
+        int max = -1;
+        char result = ' ';
+        for (char c : str.toCharArray()) {
+            if (freq[c] > max) {
+                max = freq[c];
+                result = c;
+            }
+        }
+        System.out.println("Most Frequent Character: " + result);
+    }
+}
